450a1914f96d381219ccb65f3d3912d40639826d
alexgonzalez001 <alexgr20063@gmail.com>
2026-04-09 12:42:19 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        miniCalculadora Calculadora = new miniCalculadora();
+
+        System.out.println("1. Raíz cuadrada");
+        System.out.println("2. Valor absoluto");
+        System.out.print("Elige una opción: ");
+        int opcion = sc.nextInt();
+
+        System.out.print("Introduce un número: ");
+        double numero = sc.nextDouble();
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Resultado: " + Calculadora.raizCuadrada(numero));
+                break;
+            case 2:
+                System.out.println("Resultado: " + Calculadora.valorAbsoluto(numero));
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+
+        sc.close();
     }
 }
